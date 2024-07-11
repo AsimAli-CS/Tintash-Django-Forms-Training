7d47f039a3aeba25910785b868603e0ffe1e2a9f
AsimAli-CS <asimaliii1121@gmai.com>
2024-07-11 04:36:35 -0700

    Django Generic Form

--- myapp/forms.py
+++ myapp/forms.py
@@ -3,3 +3,6 @@ from django import forms
 
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
+    your_age = forms.IntegerField(label='Your age')  # Add age fields
+   
+
